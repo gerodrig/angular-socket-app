14d2f602f1656c0562085d79e8cb84a416d2fc88
Gerardo Rodriguez <garc@outlook.com>
2023-05-31 20:09:32 -0600

    Untrack ignored files, monorepo cofig completed

--- apps/01-server-sockets/src/app.module.ts
+++ apps/01-server-sockets/src/app.module.ts
@@ -1,6 +1,10 @@
 import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+
 import { MessagesController } from './message/message.controller';
 import { MessagesService } from './message/message.service';
 import { MessagesWsModule } from './messages-ws/messages-ws.module';
@@ -11,7 +15,13 @@ import { GraphService } from './graph/graph.service';
 import { PollService } from './graph/poll.service';
 
 @Module({
-  imports: [MessagesWsModule, UsersModule],
+  imports: [
+    MessagesWsModule,
+    UsersModule,
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '../../', '01-client-sockets/dist'),
+    }),
+  ],
   controllers: [
     AppController,
     MessagesController,
